91b767457736724e3fb33d9a768da3b48dc245a4
geekduck <duck1218+github@gmail.com>
2016-01-16 09:36:09 +0900

    Add fetch.

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
+  console.dir(event);
   // インストール処理
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -36,4 +37,42 @@ $("#cachedMethodButton").on("click", function(){
 $("#uncachedMethodButton").on("click", function(){
   var $logDiv = $("<div class='log uncached'></div>");
   $(".output").append($logDiv.append(uncachedFunc()));
+});
+
+self.addEventListener('fetch', function(event) {
+  console.dir(event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュがあったのでレスポンスを返す
+        if (response) {
+          return response;
+        }
+
+        // 重要：リクエストを clone する。リクエストは Stream なので
+        // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+        // 必要なので、リクエストは clone しないといけない
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then(function(response) {
+            // レスポンスが正しいかをチェック
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 重要：レスポンスを clone する。レスポンスは Stream で
+            // ブラウザ用とキャッシュ用の2回必要。なので clone して
+            // 2つの Stream があるようにする
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
 });
